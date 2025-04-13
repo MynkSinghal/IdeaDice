b5877a6bae754cad2c23cf21fe2b46c9743e4ff0
MynkSinghal <singhal2004mayank@gmail.com>
2025-04-13 18:45:44 +0530

    Initial Commit

---
+++ IdeaDice/IdeaDiceApp.swift
@@ -0,0 +1,17 @@
+//
+//  IdeaDiceApp.swift
+//  IdeaDice
+//
+//  Created by Mayank Singhal on 13/04/25.
+//
+
+import SwiftUI
+
+@main
+struct IdeaDiceApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ IdeaDiceTests/IdeaDiceTests.swift
@@ -0,0 +1,17 @@
+//
+//  IdeaDiceTests.swift
+//  IdeaDiceTests
+//
+//  Created by Mayank Singhal on 13/04/25.
+//
+
+import Testing
+@testable import IdeaDice
+
+struct IdeaDiceTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
